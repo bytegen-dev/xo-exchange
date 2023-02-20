bb96f603ac5e176ff77c83c74420357b21f8da35
tertiux44 <secondsonofadebayo@gmail.com>
2023-02-21 00:12:49 +0100

    api integration ending

--- src/Home.js
+++ src/Home.js
@@ -55,19 +55,23 @@ export default function Home(props) {
         </div>
         <div className="featured-items-holder">
             <div className="featured--item">
-                <div>USD</div>
+                <img src="usa.png" alt="flag uk" />
+                <div><i className="fa fa-dollar"/> USD</div>
                 {formatCurrency(currencies.USD)} 
             </div>
             <div className="featured--item">
-                <div>EUR</div> 
-                {formatCurrency(currencies.EUR)}
-            </div>
-            <div className="featured--item">
-                <div>GBP</div> 
+                <img src="uk.png" alt="flag uk" />
+                <div><i className="fa fa-sterling-sign"/> GBP</div>
                 {formatCurrency(currencies.GBP)}
             </div>
             <div className="featured--item">
-                <div>CNY</div> 
+                <img src="eu.png" alt="flag uk" />
+                <div><i className="fa fa-euro"/> EUR</div>
+                {formatCurrency(currencies.EUR)}
+            </div>
+            <div className="featured--item">
+                <img src="china.png" alt="flag uk" />
+                <div><i className="fa fa-yen"/> CNY</div>
                 {formatCurrency(currencies.CNY)}
             </div>
           {/* <div className="featured--item">{formatCurrency(currencies.EUR)}</div>
